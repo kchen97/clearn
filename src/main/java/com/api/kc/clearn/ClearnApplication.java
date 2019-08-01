75189fb0542abc285e93731b32dc332f4fb38e0b
Korman Chen <korchen@ucdavis.edu>
2019-07-31 19:42:51 -0700

    project setup

---
+++ src/main/java/com/api/kc/clearn/ClearnApplication.java
@@ -0,0 +1,13 @@
+package com.api.kc.clearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearnApplication.class, args);
+	}
+
+}
